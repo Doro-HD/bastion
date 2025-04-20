ffc9000da7564c324526234ec9954d342f2bc53d
doro-hd <doro-hd@protonmail.com>
2025-04-20 19:06:55 +0200

    Use chained methods for typing

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,14 @@ import { cors } from 'hono/cors';
 import { authRouter, protectedRouter } from '@/routers';
 import { env } from './lib';
 
-const app = new Hono();
-app.use(
-	cors({
-		origin: env.ORIGIN,
-		credentials: true
-	})
-);
-
-app.route('/auth', authRouter);
-app.route('/', protectedRouter);
+const app = new Hono()
+	.use(
+		cors({
+			origin: env.ORIGIN,
+			credentials: true
+		})
+	)
+	.route('/auth', authRouter)
+	.route('/', protectedRouter);
 
 export default app;
--- server/src/lib/routers/protected/index.ts
+++ server/src/lib/routers/protected/index.ts
@@ -3,30 +3,30 @@ import { getCookie } from 'hono/cookie';
 
 import { auth } from '@/index';
 import decksRouter from './decksRouter';
+import cardsRouter from './cardsRouer';
 
 type Variables = {
 	userId: string;
 };
 
-const protectedRouter = new Hono<{ Variables: Variables }>();
+const protectedRouter = new Hono<{ Variables: Variables }>()
+	.use(async (c, next) => {
+		const sessionToken = getCookie(c, auth.sessionCookieName);
+		if (!sessionToken) {
+			return c.json({ data: 'Unauthorized' }, 401);
+		}
 
-protectedRouter.use(async (c, next) => {
-	const sessionToken = getCookie(c, auth.sessionCookieName);
-	if (!sessionToken) {
-		return c.json({ data: 'Unauthorized' }, 401);
-	}
+		const session = await auth.validateSessionToken(sessionToken);
+		if (!session.session) {
+			return c.json({ data: 'Unauthorized' }, 401);
+		}
 
-	const session = await auth.validateSessionToken(sessionToken);
-	if (!session.session) {
-		return c.json({ data: 'Unauthorized' }, 401);
-	}
+		c.set('userId', session.session.userId);
 
-	c.set('userId', session.session.userId);
-
-	await next();
-});
-
-protectedRouter.route('/decks', decksRouter);
+		await next();
+	})
+	.route('/decks', decksRouter)
+	.route('/cards', cardsRouter);
 
 export default protectedRouter;
 export { type Variables };
