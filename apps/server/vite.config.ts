5e1ffd024807c5564fa47327227c41af2443f86e
doro-hd <doro-hd@protonmail.com>
2025-04-21 14:48:13 +0200

    Attempt __dirname workaround with define

--- apps/server/vite.config.ts
+++ apps/server/vite.config.ts
@@ -3,12 +3,18 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { cloudflare } from '@cloudflare/vite-plugin'
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
 	build: {
 		minify: true,
-		rollupOptions: {
-			external: ['argon2']
-		}
+	},
+	define: {
+		__dirname: JSON.stringify(__dirname)
 	},
 	plugins: [
 		tsconfigPaths(),
