c13ae64fb1147beb1038e7010773ad6405e7a990
doro-hd <sid.doro-hd@protonmail.com>
2025-08-09 09:57:53 +0200

    Attempt to fix lint and build errors

--- apps/api/env.d.ts
+++ apps/api/env.d.ts
@@ -1,4 +1,5 @@
 declare module 'cloudflare:test' {
 	// ProvidedEnv controls the type of `import("cloudflare:test").env`
+	// @ts-ignore - Can be empty for now
 	interface ProvidedEnv extends Env {}
 }
