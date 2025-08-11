eb7f658340c13629023e1fbf83505009bb2cf040
doro-hd <sid.doro-hd@protonmail.com>
2025-08-11 10:51:39 +0200

    Add module mocking for db

--- apps/api/src/routers/authRouter/authRouter.spec.ts
+++ apps/api/src/routers/authRouter/authRouter.spec.ts
@@ -1,39 +1,55 @@
-import { describe, it, expect, vi, afterEach } from 'vitest';
-import { env } from 'cloudflare:test';
-import { faker } from '@faker-js/faker';
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import { env } from "cloudflare:test";
+import { faker } from "@faker-js/faker";
 
-import app from '@/index';
-import UserHandler from '@/db/users/handler';
-import result from '@/result';
+import app from "@/index";
+import UserHandler from "@/db/users/handler";
+import { result } from "@doro-hd/result";
 
-const basePath = '/auth';
+const basePath = "/auth";
 
 afterEach(() => vi.clearAllMocks());
 
-describe('Sign up', () => {
+beforeEach(() => {
+	vi.mock(import("@/db/index"), async (importOriginal) => {
+		const module = await importOriginal();
+		return {
+			...module,
+			default: vi.fn(),
+		};
+	});
+});
+
+describe("Sign up", () => {
 	const path = `${basePath}/sign-up`;
 
-	it('Should succeed with correct data', async () => {
+	it("Should succeed with correct data", async () => {
 		const createUserSpy = vi
-			.spyOn(UserHandler.prototype, 'createUser')
-			.mockImplementation(async (newUser) => result.ok({ id: faker.string.uuid(), ...newUser }));
+			.spyOn(UserHandler.prototype, "createUser")
+			.mockImplementation(async (newUser) =>
+				result.ok({
+					id: faker.string.uuid(),
+					...newUser,
+				})
+			);
 
 		const data = { username: faker.internet.username() };
 		const res = await app.request(
 			path,
 			{
-				method: 'post',
+				method: "post",
 				headers: {
-					'Content-Type': 'application/json'
+					"Content-Type": "application/json",
 				},
-				body: JSON.stringify(data)
+				body: JSON.stringify(data),
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(201);
 
-		const jsonData: { id: string; username: string } = await res.json();
+		const jsonData: { id: string; username: string } = await res
+			.json();
 
 		// value expectations
 		expect(jsonData.id).toBeTruthy();
@@ -43,97 +59,97 @@ describe('Sign up', () => {
 		expect(createUserSpy).toBeCalledWith(data);
 	});
 
-	it('Should fail with bad request, 400', async () => {
+	it("Should fail with bad request, 400", async () => {
 		const res = await app.request(
 			path,
 			{
-				method: 'post',
+				method: "post",
 				headers: {
-					'Content-Type': 'application/json'
+					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({})
+				body: JSON.stringify({}),
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(400);
 	});
 });
 
-describe('Sign in', () => {
-	const path = '/sign-in';
+describe("Sign in", () => {
+	const path = `${basePath}/sign-up`;
 
-	it('Should succeed with correct data', async () => {
+	it("Should succeed with correct data", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(200);
 	});
 
-	it('Should fail with bad request, 400', async () => {
+	it("Should fail with bad request, 400", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(400);
 	});
 
-	it('Should fail with unauthorized, 401', async () => {
+	it("Should fail with unauthorized, 401", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(401);
 	});
 
-	it('Should fail with user not found, 404', async () => {
+	it("Should fail with user not found, 404", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(404);
 	});
 });
 
-describe('Validate', () => {
-	const path = '/validate';
+describe("Validate", () => {
+	const path = `${basePath}/sign-up`;
 
-	it('Should succed with correct data', async () => {
+	it("Should succed with correct data", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(200);
 	});
 
-	it('Should fail with unauthorized, 401', async () => {
+	it("Should fail with unauthorized, 401", async () => {
 		const res = await app.request(
-			`${basePath}${path}`,
+			path,
 			{
-				method: 'post'
+				method: "post",
 			},
-			env
+			env,
 		);
 
 		expect(res.status).toBe(401);
