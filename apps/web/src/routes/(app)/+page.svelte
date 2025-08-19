a50e4e7b7c4e7cf0a65c0009948f37cbe3a6f213
doro-hd <doro-hd@protonmail.com>
2025-08-19 18:04:38 +0200

    Start on homepage for authorized users

---
+++ apps/web/src/routes/(app)/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import getAuthStore from '$lib/stores/authStore.svelte';
+
+	const authStore = getAuthStore();
+
+	$effect(() => {
+		if (!authStore.isAuthorized) {
+			goto('/');
+		}
+	});
+</script>
+
+<h1>Authorized</h1>
