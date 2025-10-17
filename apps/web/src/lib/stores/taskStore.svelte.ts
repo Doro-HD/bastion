d7b606b34863f5e64b129a1acc077d8948fb642f
doro-hd <doro-hd@protonmail.com>
2025-10-17 12:55:53 +0200

    Add simple task feature

---
+++ apps/web/src/lib/stores/taskStore.svelte.ts
@@ -0,0 +1,50 @@
+import { browser } from "$app/environment";
+
+interface ITask {
+	id: string;
+	name: string;
+	description: string;
+	tags: string[];
+}
+
+class TaskStore {
+	tasks: ITask[] = $state([]);
+
+	constructor() {
+		if (browser) {
+			let storedTaskJson = localStorage.getItem("tasks");
+			if (!storedTaskJson) {
+				storedTaskJson = "[]"
+			}
+
+			const storedTasks = JSON.parse(storedTaskJson);
+			this.tasks = storedTasks;
+		}
+	}
+
+	getRandomTask() {
+		const taskLength = this.tasks.length;
+		if (taskLength === 0) {
+			return;
+		}
+
+		const randomIndex = Math.floor(Math.random() * taskLength);
+
+		return this.tasks[randomIndex];
+	}
+
+	addTask(task: ITask) {
+		this.tasks = [...this.tasks, task];
+	}
+
+	removeTask(taskID: string) {
+		this.tasks = this.tasks.filter(task => task.id !== taskID);
+	}
+}
+
+const taskStore = new TaskStore();
+
+export {
+	taskStore,
+	type ITask
+}
--- apps/web/src/routes/Navbar.svelte
+++ apps/web/src/routes/Navbar.svelte
@@ -3,16 +3,26 @@
 	import { authStore } from '$lib/stores/authStore.svelte';
 	import ThemeSwitcher from './ThemeSwitcher.svelte';
 
+	let isAuthenticated = $derived(authStore.isAuthenticated);
+
 	function signOut() {
 		authService.signOut();
 	}
 </script>
 
 <div class="navbar sticky top-0 flex justify-between bg-base-100 shadow-sm">
-	<a href="/" class="btn text-2xl btn-ghost">Bastion</a>
+	<div>
+		<a href="/" class="btn text-2xl btn-ghost">Bastion</a>
+
+		{#if isAuthenticated}
+			<a href="/home" class="btn text-lg btn-ghost">Home</a>
+
+			<a href="/tasks" class="btn text-lg btn-ghost">Tasks</a>
+		{/if}
+	</div>
 
 	<div>
-		{#if authStore.isAuthenticated}
+		{#if isAuthenticated}
 			<button onclick={signOut} class="btn text-lg btn-ghost">Sign out</button>
 		{:else}
 			<a href="/login" class="btn text-lg btn-ghost">Login</a>
